36ed12435d6fc0b7207fa6d1ddb9728f0847472a
Raissadaher <157515874+Raissadaher@users.noreply.github.com>
2024-02-15 09:09:07 -0300

    Update Layout GEGEO.py

--- Layout GEGEO.py
+++ Layout GEGEO.py
@@ -8,9 +8,6 @@ from qgis.core import QgsProject, QgsCoordinateReferenceSystem
 from qgis.utils import iface
 from datetime import date
 from qgis.core import QgsLayoutItemScaleBar, QgsProject
-from qgis.PyQt.QtGui import *
-from PyQt5.QtCore import Qt
-
 
 def imprime_layers():
     names = [mapLayers.name() for mapLayers in QgsProject.instance().mapLayers().values()]
@@ -31,20 +28,15 @@ page = QgsLayoutItemPage(layout)
 page.setPageSize('A4', QgsLayoutItemPage.Landscape)
 layout.pageCollection().addPage(page)
 
-# # removendo camadas indesejadas
-# mapa_layers_desejados = ["Reserva Legal", "Centroides gerais", "Área de supressão", "Área de Preservação Permanente", "Área do imóvel", "Área de supressão em APP", "Área de supressão em RL", "Danificar floresta ou '\n' qualquer tipo de vegetação nativa"]
-# layers_indesejados = [i for i in project.mapLayers().values() if i.name() not in mapa_layers_desejados]
-# for layer_indesejada in layers_indesejados:
-#     project.removeMapLayer(layer_indesejada)
-# # imprime_layers()
 
 # Adicionando Poligono Maior
+
 rectangle_maior = QgsLayoutItemShape(layout)
 rectangle_maior.setShapeType(QgsLayoutItemShape.Rectangle)
 rectangle_maior.setRect(0, 0, 0, 0)
 layout.addLayoutItem(rectangle_maior)
-rectangle_maior.attemptMove(QgsLayoutPoint(5, 5, QgsUnitTypes.LayoutMillimeters))
-rectangle_maior.attemptResize(QgsLayoutSize(287.5, 200, QgsUnitTypes.LayoutMillimeters))
+rectangle_maior.attemptMove(QgsLayoutPoint(6, 3, QgsUnitTypes.LayoutMillimeters))
+rectangle_maior.attemptResize(QgsLayoutSize(287.717, 202.350, QgsUnitTypes.LayoutMillimeters))
 iface.openLayoutDesigner(layout)
 
 # Adicionando Poligono Titulo
@@ -61,11 +53,10 @@ mapa.setAtlasDriven(True)
 mapa.setFrameEnabled(True)
 mapa.setRect(0, 0, 200, 188)
 canvas = iface.mapCanvas()
-#mapa.setExtent(canvas.extent())
-mapa.zoomToExtent(canvas.extent())
+mapa.setExtent(canvas.extent())
 layout.addLayoutItem(mapa)
 mapa.attemptMove(QgsLayoutPoint(10, 8, QgsUnitTypes.LayoutMillimeters))
-mapa.attemptResize(QgsLayoutSize(200, 193, QgsUnitTypes.LayoutMillimeters))
+mapa.attemptResize(QgsLayoutSize(200, 193.3, QgsUnitTypes.LayoutMillimeters))
 
 # Criando Legenda
 legenda = QgsLayoutItemLegend(layout)
@@ -74,25 +65,25 @@ legenda_fonte = QFont("Arial", 11)
 legenda.setStyleFont(QgsLegendStyle.Title, legenda_fonte)
 legenda.setStyleFont(QgsLegendStyle.Subgroup, legenda_fonte)
 legenda.setStyleFont(QgsLegendStyle.SymbolLabel, legenda_fonte)
-legenda.attemptMove(QgsLayoutPoint(212, 65, QgsUnitTypes.LayoutMillimeters))
+legenda.attemptMove(QgsLayoutPoint(212, 90, QgsUnitTypes.LayoutMillimeters))
 legenda.attemptResize(QgsLayoutSize(20, 20, QgsUnitTypes.LayoutMillimeters))
-legenda.setItemOpacity(1.0)
-legenda.setBackgroundEnabled(1)
 legenda.setLinkedMap(mapa)
+legenda.setItemOpacity(1.0)
+legenda.setBackgroundEnabled(0.5)
 legenda.setLegendFilterByMapEnabled(True)
 legenda.refresh()
 layout.addLayoutItem(legenda)
 
-# Isso adiciona rótulos ao mapa
+# Isso adiciona titulo ao mapa
 layer = project.mapLayersByName('Área do imóvel')[0]
 nome = ""
 proprietario = ""
-minicipio = ""
+municipio = ""
 cpf = ""
 wkt = ""
 # Verifica se a camada está válida
 if layer is not None:
-    # Itera sobre as features selecionadas
+ # Itera sobre as features selecionadas
     for feature in layer.getFeatures():
         nome = feature['nm_imovel']
         proprietario = feature['proprietar']
@@ -108,9 +99,10 @@ title.setFont(QFont("Arial", 10))
 title.adjustSizeToText()
 title.setHAlign(Qt.AlignCenter)
 layout.addLayoutItem(title)
-title.attemptResize(QgsLayoutSize(75, 60, QgsUnitTypes.LayoutMillimeters))
-title.attemptMove(QgsLayoutPoint(215, 35, QgsUnitTypes.LayoutMillimeters))
+title.attemptResize(QgsLayoutSize(75, 50, QgsUnitTypes.LayoutMillimeters))
+title.attemptMove(QgsLayoutPoint(213, 35, QgsUnitTypes.LayoutMillimeters))
 
+# Isso adiciona subtitulo ao mapa
 dados_propriedade_subtitulo = f"""Sistema Geodésico: Sirgas 2000
 Sistema de coordenada: Projetado-UTM
 Formato: A4
@@ -123,22 +115,26 @@ Data : {date.today().strftime("%d/%m/%Y")}
 
 subtitle = QgsLayoutItemLabel(layout)
 subtitle.setText(dados_propriedade_subtitulo)
-subtitle.setFont(QFont("Arial", 12))
+subtitle.setFont(QFont("Arial", 10))
 subtitle.setRect(0, 0, 0, 0)
 subtitle.adjustSizeToText()
+subtitle.setHAlign(Qt.AlignCenter)
 layout.addLayoutItem(subtitle)
-subtitle.attemptMove(QgsLayoutPoint(215, 150, QgsUnitTypes.LayoutMillimeters))
-subtitle.attemptResize(QgsLayoutSize(70, 50, QgsUnitTypes.LayoutMillimeters))
+subtitle.attemptMove(QgsLayoutPoint(215, 165, QgsUnitTypes.LayoutMillimeters))
+subtitle.attemptResize(QgsLayoutSize(70, 37, QgsUnitTypes.LayoutMillimeters))
 
 # inserir o Norte
 north = QgsLayoutItemPicture(layout)
 north.setPicturePath("C:\\Users\\raissa.alves\\Documents\\Atlas\\norte.svg")
 layout.addLayoutItem(north)
-north.attemptMove(QgsLayoutPoint(8, 5, QgsUnitTypes.LayoutMillimeters))
+north.attemptMove(QgsLayoutPoint(240, 70, QgsUnitTypes.LayoutMillimeters))
 north.attemptResize(QgsLayoutSize(20, 20, QgsUnitTypes.LayoutMillimeters))
 
 # inserir a logo
+
+caminho_base = "C:/Users/raissa.alves/Documents/Atlas/",
 north = QgsLayoutItemPicture(layout)
+north.setPicturePath("Semad_Abreviada_positivo.png")
 north.setPicturePath("C:/Users/raissa.alves/Documents/Atlas/Semad_Abreviada_positivo.png")
 layout.addLayoutItem(north)
 north.attemptMove(QgsLayoutPoint(214, 10, QgsUnitTypes.LayoutMillimeters))
@@ -149,37 +145,32 @@ area_imovel = project.mapLayersByName('Área do imóvel')[0]
 CAR = ""
 # Verifica se a camada está válida
 if area_imovel is not None:
-    # Itera sobre as features selecionadas
+# Itera sobre as features selecionadas
     for feature in area_imovel.getFeatures():
         CAR = feature['cod_imovel']
 
 dados = 'CAR: ' + CAR
 title = QgsLayoutItemLabel(layout)
 title.setText(dados)
-title.setFont(QFont("Arial", 9))
+title.setFont(QFont("Arial", 7))
 title.adjustSizeToText()
 title.setHAlign(Qt.AlignLeft)
 layout.addLayoutItem(title)
-title.attemptResize(QgsLayoutSize(100, 4, QgsUnitTypes.LayoutMillimeters))
-title.attemptMove(QgsLayoutPoint(10, 201, QgsUnitTypes.LayoutMillimeters))
+title.attemptResize(QgsLayoutSize(75, 4, QgsUnitTypes.LayoutMillimeters))
+title.attemptMove(QgsLayoutPoint(10, 202, QgsUnitTypes.LayoutMillimeters))
 
 # inserindo grid
+
 grid = QgsLayoutItemMapGrid("New grid", mapa)
 mapa.grid().setEnabled(True)
 mapa.grid().setUnits(QgsLayoutItemMapGrid.DynamicPageSizeBased)
-# map.grid().setIntervalX(1000)
-# map.grid().setIntervalY(1000)
-mapa.grid().setMaximumIntervalWidth(100)
-mapa.grid().setMinimumIntervalWidth(50)
+mapa.grid().setMaximumIntervalWidth(50)
+mapa.grid().setMinimumIntervalWidth(45)
 mapa.grid().setAnnotationEnabled(True)
-# map.grid().setGridLineColor(QColor(0, 176, 246))
 mapa.grid().setGridLineWidth(0.1)
 mapa.grid().setAnnotationPrecision(0)
 mapa.grid().setAnnotationFrameDistance(1)
 mapa.grid().setAnnotationFontColor(QColor(0, 0, 0))
-# map.grid().setStyle(QgsLayoutItemMapGrid.FrameAnnotationsOnly)
-# map.grid().setStyle(QgsLayoutItemMapGrid.Cross)
-# map.grid().setStyle(QgsLayoutItemMapGrid.Markers)
 mapa.grid().setAnnotationDisplay(QgsLayoutItemMapGrid.HideAll, QgsLayoutItemMapGrid.Right)
 mapa.grid().setAnnotationDisplay(QgsLayoutItemMapGrid.ShowAll, QgsLayoutItemMapGrid.Top)
 mapa.grid().setAnnotationDisplay(QgsLayoutItemMapGrid.HideAll, QgsLayoutItemMapGrid.Bottom)
@@ -193,17 +184,27 @@ mapa.grid().setAnnotationDisplay(QgsLayoutItemMapGrid.ShowAll, QgsLayoutItemMapG
 escala = QgsLayoutItemScaleBar(layout)
 escala.setStyle('Caixa Simples')
 escala.setLinkedMap(mapa)
+escala.setHeight(2)
+escala.setFont(QFont('Arial', 10))
 escala.applyDefaultSize()
-escala.attemptMove(QgsLayoutPoint(230, 135, QgsUnitTypes.LayoutMillimeters))
+escala.attemptMove(QgsLayoutPoint(240, 150, QgsUnitTypes.LayoutMillimeters))
 layout.addLayoutItem(escala)
 
+
 # Adicionando a tabela de atributos
-layer = project.mapLayersByName('Gerais')[0]
-pdf_table = QgsLayoutItemAttributeTable.create(layout)
-pdf_table.setVectorLayer(layer)
-pdf_table.setMaximumNumberOfFeatures(40)
-frame2 = QgsLayoutFrame(layout, pdf_table)
+layer = project.mapLayersByName('Centroides gerais')[0]
+tabela_atributos = QgsLayoutItemAttributeTable.create(layout)
+tabela_atributos.setVectorLayer(layer)
+tabela_atributos.setMaximumNumberOfFeatures(40)
+layout.addMultiFrame(tabela_atributos)
+frame2 = QgsLayoutFrame(layout, tabela_atributos)
 frame2.attemptResize(QgsLayoutSize(198.200, 224), True)
 frame2.attemptMove(QgsLayoutPoint(120, 220, QgsUnitTypes.LayoutMillimeters))
-pdf_table.addFrame(frame2)
-pdf_table.setResizeMode(1)
+tabela_atributos.addFrame(frame2)
+tabela_atributos.setResizeMode(1)
+
+coluna = QgsLayoutTableColumn()
+coluna.setAttribute('id') # First column name in your case. You can also put an expression
+coluna.setSortOrder(0) # 0 = Asc, 1 Desc
+tabela_atributos.setSortColumns([coluna])
+layout.refresh()
